fdc2c5db554b491aa4b2eaa5ed9caef69437d174
Jed <lejosnej@ainfosec.com>
2016-04-05 16:07:09 -0400

    windows: cleanup and improve scripts
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- windows/BuildDaemon/winbuildd.py
+++ windows/BuildDaemon/winbuildd.py
@@ -139,17 +139,17 @@ class RPCInterface(object):
                 result = 'FAILURE'
         finally:
             if log is not None:
-                log.handlers[0].close()
-
-        if log is not None:
-            log.handlers[0].close()
+                handlers = log.handlers[:]
+                for handler in handlers:
+                    handler.close()
+                    log.removeHandler(handler)
 
         return result
 
     def hello(self):
         return "hello back"
 
-    def getcert(self):
+    def get_ssh_public_key(self):
         certfile = open(os.path.join(SCRIPTDIR, "id_rsa.pub"))
         cert = certfile.readline()
         certfile.close()
